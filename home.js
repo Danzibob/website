4520254d03d667e2b57fd41b9a7c889d7c11ed4b
Danzibob <Danzibob@gmail.com>
2016-09-05 12:22:39 +0100

    Added navbar links and background

--- home.js
+++ home.js
@@ -2,16 +2,62 @@ $(document).ready(function() {
 	$("#header").load("http://danzibob.tk/header.html");	
 });
 
+$(window).resize(function(){
+    var newwidth = $(window).width();
+    var newheight = $(window).height();      
+    $("canvas").height(newheight).width(newwidth);
+});
+
+
+
+var cells = [];
+var W = 40;
+
 function setup() {
 	var cnv = createCanvas(windowWidth, windowHeight);
 	cnv.position(0, 0);
+	
+	for(var i = -1; i < width/W + 1; i ++){
+		for(var j = -1; j < height/W + 1; j ++){
+			cells.push(new Cell(i,j));
+		}
+	}
+	
+	
+	colorMode(HSB,100);
 	noFill();
 	stroke(255);
 	strokeWeight(4);
-	background(51);
+	background(0,0,20);
 }
 
 function draw() {
-	background(51);
-	ellipse(mouseX, mouseY, 300);
+	background(0,0,20);
+	for(var i = 0; i < cells.length; i++){
+		cells[i].increment();
+		cells[i].show();
+	}
+}
+
+
+
+var Cell = function(x,y){
+	this.x = x*W;
+	this.y = y*W;
+	this.hue = 0;
+	this.show = function(){
+		noStroke();
+		fill(this.hue,80,80)
+		rect(this.x , this.y , W-2, W-2, 5);
+	}
+	this.increment = function(){
+		var d = dist(this.x + W/2, this.y + W/2, mouseX, mouseY);
+		if(d < 300){
+			d = 60/(d+50) -0.18;
+			this.hue += d;
+		}
+		if(this.hue >= 100){
+			this.hue = this.hue%100;
+		}
+	}
 }
