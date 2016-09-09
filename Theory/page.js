7f4d2a1d99bd01026f36bfa8e07699bad3d83dbd
Danzibob <Danzibob@gmail.com>
2016-09-09 09:35:27 +0100

    Updated old theory and created new theory layout

---
+++ Theory/page.js
@@ -0,0 +1,23 @@
+var scrolled;
+$(document).ready(function() {
+    $("#header").load("../header.html");
+    var scroll = $(window).scrollTop();
+    if(scroll == 0){
+        scrolled = false;
+    } else {
+        scrolled = true;
+        $("#main_header").hide().animate({left:"110vw"}, 500);
+    }
+});
+
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    $("#main_header").attr("top",scroll*-1)
+    if(scrolled && scroll == 0){
+        $("#main_header").fadeIn(400);
+        scrolled = false;
+    } else if(!scrolled && scroll != 0){
+        $("#main_header").fadeOut(400);
+        scrolled = true;
+    }
+});
