49a47852daaee6c5319b818331e038ce00cf501a
Danzibob <Danzibob@gmail.com>
2016-09-27 09:48:18 +0100

    finished up S2

--- ProgSoc/Scripts/S2.js
+++ ProgSoc/Scripts/S2.js
@@ -16,3 +16,39 @@ var s = function(p){
     }
 }
 var inst1 = new p5(s,"S2-1");
+
+var s = function(p){
+    var points = []
+    p.setup = function(){
+        p.createCanvas(400,200)
+    }
+    p.draw = function(){
+        p.background(255);
+        p.textSize(20);
+        p.text(String(points),10,10,300, 200);
+    }
+    p.mousePressed = function(){
+        points.push(String([p.mouseX, p.mouseY]))
+    }
+}
+var inst2 = new p5(s,"S2-2");
+
+var s = function(p){
+    var points = []
+    p.setup = function(){
+        p.createCanvas(400,200)
+        p.stroke(255,0,0);
+        p.strokeWeight(10);
+    }
+    p.draw = function(){
+        p.background(255);
+        for(var i = 0; i < points.length; i++){
+            p.point(points[i].x, points[i].y)
+        }
+    }
+    p.mousePressed = function(){
+        points.push(p.createVector(p.mouseX, p.mouseY))
+        if(points.length > 10){points.splice(0,1);}
+    }
+}
+var inst2 = new p5(s,"S2-3");
